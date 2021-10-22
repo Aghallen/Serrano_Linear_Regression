9fdcd533e20dbc600b6ec271740bc344480a57ef
Håkan Pettersson <hakan.pettersson.mmx@gmail.com>
2021-10-22 09:52:50 +0200

    Changed from data class to ordinary class.

---
+++ window.py
@@ -0,0 +1,6 @@
+class Window:
+    def __init__(self, xmin=-100, xmax=100, ymin=-100, ymax=100):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
